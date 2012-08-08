7bbf31ae557a1f26b66c6b911694cba970b13684
kim tae kwang <eroica7@gmail.com>
2012-08-08 15:30:25 +0900

    최초 commit

---
+++ GuestBook/src/guestbook/Employee.java
@@ -0,0 +1,16 @@
+package guestbook;
+import com.google.appengine.api.datastore.Key;
+
+import javax.jdo.annotations.PersistenceCapable;
+import java.util.Date;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PrimaryKey;
+
+
+@PersistenceCapable
+public class Employee {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+}
