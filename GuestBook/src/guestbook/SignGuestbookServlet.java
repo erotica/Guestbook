4dfc9ee8f5f3d0c9e16f9f506bad681b31cf0f81
kim tae kwang <eroica7@gmail.com>
2012-11-23 14:42:54 +0900

    11월23일

--- GuestBook/src/guestbook/SignGuestbookServlet.java
+++ GuestBook/src/guestbook/SignGuestbookServlet.java
@@ -3,8 +3,10 @@ package guestbook;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
